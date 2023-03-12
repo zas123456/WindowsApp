5f6d909b5535c884f5b996fc0838375600f8df08
zas123456 <127037460+zas123456@users.noreply.github.com>
2023-03-12 22:38:35 +0800

    Add files via upload
    Second—2.1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public void Primnum(int n)
+    {
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+                return;
+        }
+        Console.WriteLine(n);
+    }
+    static void Main(string[] args)
+    {
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The Primenumber of " + m + " is:");
+        for (int j = 2; j < m; j++)
+        {
+            if (m % j == 0)
+            {
+                Program program = new Program();
+                program.Primnum(j);
+            }
+
+        }
+
+    }
+
+}
