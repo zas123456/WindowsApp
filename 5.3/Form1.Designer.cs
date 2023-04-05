a8e1fe692a00cb2afbecc5454e029053f996b652
zas123456 <127037460+zas123456@users.noreply.github.com>
2023-04-05 16:54:56 +0800

    Add files via upload

---
+++ 5.3/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace homework5._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(73, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(221, 118);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(73, 31);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(220, 36);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "保存编辑文本";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "打开文本文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "创建文本文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(361, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "合并文本文件";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "未创建";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "未创建";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "未创建";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "未创建";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(212, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "未创建";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(338, 316);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "未创建";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(430, 269);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(70, 44);
+            this.checkedListBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ 5.3/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace homework5._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string directory_path = "C:\\Users\\lenovo\\source\\repos\\homework5.3\\homework5.3\\files\\";
+        public string DirectoryPath;
+        char file = (char)1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //打开文本文件
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DirectoryPath = directory_path + richTextBox2.Text.Trim() + ".txt";
+                FileStream textfile = File.Open(DirectoryPath, FileMode.OpenOrCreate, FileAccess.Read);
+                ArrayList arr=new ArrayList();
+
+                byte[]bytes=new byte[textfile.Length];
+                textfile.Read(bytes, 0, bytes.Length);
+                string str=Encoding.UTF8.GetString(bytes);
+                richTextBox1.Text += str;
+                textfile.Close();
+            }
+            catch (Exception mm)
+            {
+                MessageBox.Show("文件读取失败" + Convert.ToString(mm), "消息");
+            }
+        }
+
+        //保存编辑文本
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DirectoryPath = directory_path + richTextBox2.Text.Trim() + ".txt";
+            try
+            {
+                FileStream textfile=File.Open(DirectoryPath, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(textfile, Encoding.GetEncoding("GB2312"));
+                string str = richTextBox1.Text;
+                byte[]bytes=Encoding.UTF8.GetBytes(str);
+                textfile.Write(bytes, (int)textfile.Length, richTextBox1.Text.Length);
+                switch(file)
+                {
+                    case (char)1:
+                        label1.Text = richTextBox2.Text;
+                        checkedListBox1.Items.Add(richTextBox2.Text);break;
+                    case (char)2:
+                        label2.Text = richTextBox2.Text;
+                        checkedListBox1.Items.Add(richTextBox2.Text); break;
+                    case (char)3:
+                        label3.Text = richTextBox2.Text;
+                        checkedListBox1.Items.Add(richTextBox2.Text); break;
+                    case (char)4:
+                        label4.Text = richTextBox2.Text;
+                        checkedListBox1.Items.Add(richTextBox2.Text); break;
+                    case (char)5:
+                        label5.Text = richTextBox2.Text;
+                        checkedListBox1.Items.Add(richTextBox2.Text); break;
+                    case (char)6:
+                        label6.Text = richTextBox2.Text;
+                        checkedListBox1.Items.Add(richTextBox2.Text); break;
+                }
+                file++;
+                MessageBox.Show("文件写成功", "警报");
+                textfile.Close();
+            }
+            catch(Exception mm)
+            {
+                MessageBox.Show("文件未能建立" + Convert.ToString(mm), "消息");
+            }
+        }
+
+        //创建文本文件
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(richTextBox2.Text.Length==0)
+                {
+                    MessageBox.Show("文件名禁止为空", "警报");
+                }
+                else
+                {
+                    DirectoryPath = directory_path + richTextBox2.Text.Trim() + ".txt";
+                    StreamWriter sw = File.CreateText(DirectoryPath);
+                    MessageBox.Show("文件成功建立", "消息");
+                    sw.Close();
+                }
+            }
+            catch(Exception mm)
+            {
+                MessageBox.Show("文件未能建立" + Convert.ToString(mm), "消息");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
