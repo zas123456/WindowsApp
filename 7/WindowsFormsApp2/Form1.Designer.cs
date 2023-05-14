82dafb60698f7b758a44af460a80c2d041e96092
zas123456 <127037460+zas123456@users.noreply.github.com>
2023-05-14 15:56:36 +0800

    Add files via upload

---
+++ 7/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxhtml = new System.Windows.Forms.TextBox();
+            this.textBoxurl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(478, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "抓取网络源码";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxhtml
+            // 
+            this.textBoxhtml.Location = new System.Drawing.Point(95, 138);
+            this.textBoxhtml.Multiline = true;
+            this.textBoxhtml.Name = "textBoxhtml";
+            this.textBoxhtml.Size = new System.Drawing.Size(261, 238);
+            this.textBoxhtml.TabIndex = 2;
+            // 
+            // textBoxurl
+            // 
+            this.textBoxurl.Location = new System.Drawing.Point(423, 138);
+            this.textBoxurl.Multiline = true;
+            this.textBoxurl.Name = "textBoxurl";
+            this.textBoxurl.Size = new System.Drawing.Size(262, 238);
+            this.textBoxurl.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "bing抓取的网页源码";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(420, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "url";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(637, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "显示url";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxurl);
+            this.Controls.Add(this.textBoxhtml);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxhtml;
+        private System.Windows.Forms.TextBox textBoxurl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 7/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public delegate void ProcessDelegate();
+
+        public static Queue<string> q = new Queue<string>();
+
+        public static int j = 0;
+        public static object locker=new object();
+        public static object locker1=new object();
+        public static object locker2 = new object();
+
+        public static Stopwatch watch=new Stopwatch();
+        public static string textbox=string.Empty;
+        public int Num = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urlToCrawl = ("http://cn.bing.com/search?q=" + textBox1.Text);
+
+            //HTTP请求
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(urlToCrawl);
+            //GET方法
+            req.Method = "GET";
+            //获得HTTP回复
+            HttpWebResponse resp=(HttpWebResponse)req.GetResponse();
+            //定义编码方式
+            string htmlCharset = "utf-8";
+            //编码方式
+            Encoding htmlEncoding =Encoding.GetEncoding(htmlCharset);
+            StreamReader sr = new StreamReader(resp.GetResponseStream(), htmlEncoding);
+            //显示html内容
+            string resphtml = sr.ReadToEnd();
+            textBoxhtml.Text= resphtml;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //找到网站建设类目下面的连接
+            string h1userp = @"((http|ftp|https)://)(([a-zA-Z0-9\._-]+\.[a-zA-Z]{2,6})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,4})*(/[a-zA-Z0-9\&%_\./-~-]*)?";
+
+            //捕获匹配
+            MatchCollection foundH1user = (new Regex(h1userp)).Matches(textBoxhtml.Text);
+            foreach(Match m in foundH1user)
+            {
+                string url = "http://cn.bing.com/search?q=" + (string)m.Value;
+
+                q.Enqueue(url);
+            }
+
+            Thread[] downloadThread;//声明下载线程
+            downloadThread = new Thread[21];//为线程申请资源，确定线程总数
+            watch.Start();
+            for(int i=0;i<Num;i++)
+            {
+                ThreadStart startDownload = new ThreadStart(DownLoad);
+                downloadThread[i]=new Thread(startDownload);//指定线程起始设置
+                downloadThread[i].Start();//逐个开启线程
+            }
+            //while (q.Count!=0);
+            watch.Stop();
+        }
+
+        public string wenben(string rrh)
+        {
+            string rh = rrh;
+            //抓取标题
+            string bt = @"<h2>(.*)</h2>";
+            Match nnn = (new Regex(bt)).Match(rh);
+
+            Regex hregex = new Regex("<.+?>", RegexOptions.IgnoreCase);
+            string hstrOutput = hregex.Replace(nnn.Value, "");//替换掉"<"和">"之间的内容
+            hstrOutput = hstrOutput.Replace("<", "");
+            hstrOutput = hstrOutput.Replace(">", "");
+            hstrOutput = hstrOutput.Replace(" ", "");
+
+            return hstrOutput;
+        }
+        public void DownLoad()
+        {
+            while(true)
+            {
+                string url;
+                string h2;
+                if(q.Count!=0)
+                {
+                    lock(locker1)
+                    {
+                        url= q.Dequeue();
+                        j++;
+                    }
+                    try
+                    {
+                        HttpWebRequest rr=(HttpWebRequest)WebRequest.Create(url);
+                        rr.Method = "GET"; 
+                        HttpWebResponse resp=(HttpWebResponse)rr.GetResponse();
+
+                        string htmlCharset = "utf-8";
+                        Encoding htmlEncoding=Encoding.GetEncoding(htmlCharset);
+                        StreamReader sr = new StreamReader(resp.GetResponseStream(), htmlEncoding);
+                        string rh = sr.ReadToEnd();
+                        h2 = wenben(rh);
+                        lock(locker2)
+                        {
+                            ProcessDelegate showProcess = delegate ()
+                              {
+                                  textBoxurl.AppendText(url + h2 + DateTime.Now.ToString() + "\n");
+                              };
+                            textBoxurl.Invoke(showProcess);
+
+                        }
+                    }
+                    catch(Exception eee)
+                    {
+
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
